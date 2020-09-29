6f90b7e93d31ecd15e462d07d64603e9430f18d7
AMikic33 <amikic33@hotmail.com>
2020-09-29 16:53:26 +0000

    search sityjs

--- scripts/map.js
+++ scripts/map.js
@@ -1,7 +1,7 @@
 
 
 
-/* initiating and displaying the map element */
+// initiating and displaying the map element 
 
 
     function initMap() {
@@ -13,7 +13,8 @@
             }
         });
 
-        
+         // display city
+
         var input = document.getElementById('searchInput');
 
         var autocomplete = new google.maps.places.Autocomplete(input);
@@ -36,40 +37,10 @@
                 map.setZoom(13);
             }
 
-            marker.setIcon(({
-                url: place.icon,
-                size: new google.maps.size(71,71),
-                origin: new google.maps.Point(0,0),
-                anchor: new google.maps.Point(17,34),
-                scaledSize: new google.maps.Size(35,35)
 
-            }));
             marker.setPosition(place.geometry.location);
             marker.setVisible(true);
 
-            var address = '';
-            if (place.address_components) {
-                address = [
-                    (place.address_components[0] && place.address_components[0].short_name || ''),
-                    (place.address_components[1] && place.address_components[1].short_name || ''),
-                    (place.address_components[2] && place.address_components[2].short_name || '')
-                    ].join('');
-            }
-
-            infowindow.setContent('<div><strong>' + place.name + '</strong>' + address);
-            infowindow.open(map, marker);
-
-            // location details
-
-            for (var i = 0; i< place.address_components.length; i++) {
-                if(place.address_components[i].types[0] == 'postal_code'){
-                    document.getElementById('postal_code').innerHTML = place.address_components[i].long_name;
-                }
-                if(place.address_components[i].types[0] == 'country'){
-                    document.getElementById('country').innerHTML = place.address_components[i].long_name;
-                }
-            }
-
-            document.getElementById('location').innerHTML = place.formatted_address;
+            
             });
     }
