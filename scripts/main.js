3d620e871f303a9ccce481a4c6871a90c40df5e5
AMikic33 <amikic33@hotmail.com>
2020-10-01 10:17:22 +0000

    search location js updated

--- scripts/main.js
+++ scripts/main.js
@@ -22,10 +22,22 @@ document.getElementById('museum').onclick = function() {
     buttonSelectSearchType(this.id);
 };
 
+document.getElementById('stadium').onclick = function() {
+    buttonSelectSearchType(this.id);
+};
+
 document.getElementById('city_hall').onclick = function() {
     buttonSelectSearchType(this.id);
 };
 
-document.getElementById('all').onclick = function() {
-    buttonSelectSearchType(this.id);
+
+document.getElementById('resetSearch').onclick = function() {
+    resetSearch();
 };
+
+
+function resetFields() {
+    document.getElementById('textLocation').innerHTML = "None";
+    document.getElementById('textInterest').innerHTML = "None";
+    $("#searchForm")[0].reset();
+}
--- scripts/map.js
+++ scripts/map.js
@@ -1,16 +1,22 @@
     // declaring variables
 
+    var map;
     var center;
+    var search = [];
+    var infowindow;
     var updatedType = [];
     var marker = [];
-    var search = [];
+    var markersClear = [];
+    var place = "";
+    var searchResults = [];
+    var city = [];
     
     // objects for searching interests
 
     var searchOne = { id:"library", title: "Library", typeList:['library']};
     var searchTwo = { id:"art_gallery", title: "Art Gallery", typeList:['art_gallery']};
     var searchThree = { id:"museum", title: "Museum", typeList:['museum']};
-    var searchFour = { id:"book-_store", title: "Book Store", typeList:['book_store']};
+    var searchFour = { id:"book_store", title: "Book Store", typeList:['book_store']};
     var searchFive = { id:"movie_theater", title: "movie Theater", typeList:['movie_theater']};
     var searchSix = { id:"stadium", title: "Stadium", typeList:['stadium']};
     var searchSeven = { id:"attraction", title: "Tourist Attraction", typeList:['tourist_attraction']};
@@ -24,49 +30,17 @@
   
 
     function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
+            map = new google.maps.Map(document.getElementById('map'), {
             zoom: 3,
             center: {
             lat: 46.619261,
             lng: -33.134766
             }
         });
-    
+        
+        searchSelectCity();
         buttonSelectSearchType();
 
-         // display city
-
-        var input = document.getElementById('searchInput');
-
-        var autocomplete = new google.maps.places.Autocomplete(input);
-        autocomplete.bindTo('bounds', map);
-
-        var infowindow = new google.maps.InfoWindow();
-            var marker = new google.maps.Marker({
-            map: map,
-            anchorPoint: new google.maps.Point(0, -29)
-        });
-        
-
-
-        autocomplete.addListener('place_changed', function() {
-            infowindow.close();
-            marker.setVisible(false);
-            var place = autocomplete.getPlace();
-            
-            if(place.geometry.viewport){
-                map.fitBounds(place.geometry.viewport);
-            } else {
-                map.setCenter(place.geometry.location);
-                map.setZoom(13);
-            }
-
-
-            marker.setPosition(place.geometry.location);
-            marker.setVisible(true);
-
-            
-            });
             }
 
                 // interests
@@ -74,21 +48,50 @@
 
     function buttonSelectSearchType(buttonID) {
     var i = 0;
-    
-    for (i = 0; i < search.length; i++) {
 
-        //if statement ... if element clicked id = search[i].id then we change the items to show the chosen item
+    for (i = 0; i < search.length; i++) {
+     
         if (search[i].id == buttonID) {
+          
             document.getElementById('textInterest').innerHTML = search[i].title;
             updatedType = search[i].typeList;
         }
     }
 }
 
+function searchSelectCity() {
+
+    var restrictionSet = {
+        types: ['(cities)'],
+    };
+
+    var typeCity = document.getElementById('locationName');
+    var autocomplete = new google.maps.places.Autocomplete(typeCity, restrictionSet);
+    var updateLatLng;
+
+    google.maps.event.addListener(autocomplete, 'place_changed', function() {
+        place = autocomplete.getPlace();
+    });
+
+    /* when the submit button is clicked in the cityForm, the map center is changed to this new location and the values are set using the DOM */
+    searchForm.addEventListener("submit", function() {
+        if (place !== "") {
+            updateLatLng = new google.maps.LatLng(place.geometry.location.lat(), place.geometry.location.lng());
+            map.setZoom(13);
+            center = updateLatLng;
+            map.setCenter(center);
+            document.getElementById('textLocation').innerHTML = place.adr_address;
+        }
+        else {
+            alert("Please enter location");
+        }
+    });
+}
+
 function requestLocations() {
     clearMap();
  
-    if (document.getElementById('textInterest').innerHTML != "None") {
+    if (document.getElementById('textLocation').innerHTML != "None" && document.getElementById('textInterest').innerHTML != "None") {
 
         map.setCenter(center);
 
@@ -102,8 +105,12 @@ function requestLocations() {
         service.nearbySearch(request, callback);
     }
 
-    
+        else {
+            alert("Please select a city AND a search type before searching");
+    }
 }
+
+
 
 function callback(results, status,) {
     searchResults = [];
@@ -112,7 +119,6 @@ function callback(results, status,) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
         for (var i = 0; i < searchResults.length; i++) {
             createMarker(searchResults[i]);
-            resultsTextDisplay(searchResults[i]);
         }
     }
 }
@@ -131,4 +137,13 @@ function createMarker(place) {
         infowindow.setContent(`<h4>${place.name}</h4><p><b>Type:</b> ${document.getElementById('textInterest').innerHTML}</p>`);
         infowindow.open(map, this);
     });
+}
+
+function resetSearch() {
+    place = "";
+    center = "";
+    updatedType = [null];
+
+    clearMap();
+    resetFields();
 }
